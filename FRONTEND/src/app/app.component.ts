79a7857801d43ca7ee321ea26853378c15a33b74
João Mendes <joaovictormendeslima4@gmail.com>
2025-03-19 23:03:14 -0300

    Criação do frontend e da API

---
+++ FRONTEND/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Tarefa } from './tarefa';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'TODOapp';
+  arrayDeTarefas: Tarefa[] = [];
+  constructor() {
+    this.READ_tarefas();
+  }
+
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    this.arrayDeTarefas.unshift(novaTarefa);
+  }
+
+  DELETE_tarefa(tarefaASerRemovida: Tarefa) {
+    this.arrayDeTarefas.splice(
+      this.arrayDeTarefas.indexOf(tarefaASerRemovida),
+      1,
+    );
+  }
+
+  READ_tarefas() {
+    this.arrayDeTarefas = [
+      new Tarefa('Estudar Frameworks WEB', false),
+      new Tarefa('Comer Pizza', false),
+      new Tarefa('Ajudar meus pais', false),
+    ];
+  }
+}
