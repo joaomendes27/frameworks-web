67724ec5a01589c0ffde64d4267565a5dd201466
João Mendes <joaovictormendeslima4@gmail.com>
2025-05-21 02:12:37 -0300

    Gerenciando usuários

--- BACKEND/API/index.js
+++ BACKEND/API/index.js
@@ -5,7 +5,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/', userRoutes);
 
 
-// Configuração do CORS
+// Configuração do COR
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH, DELETE');
